52d68c8e9af3ed2dc038cfcc7f094630ed511f88
SamuelDouay <douaysamuel@gmail.com>
2025-11-19 16:22:07 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header>
+
+    <Button>TEST</Button>
+
+<Checkbox binary />
+  </header>
+
+  <FloatLabel variant="on">
+    <InputText id="on_label"  autocomplete="off" />
+    <label for="on_label">On Label</label>
+</FloatLabel>
+</template>
+
+<style scoped></style>
