c304fac489d12bf5bdf79707d12b4339a5bf4804
SamuelDouay <douaysamuel@gmail.com>
2025-11-26 11:55:09 +0100

    add .env

--- vite.config.ts
+++ vite.config.ts
@@ -1,40 +1,45 @@
 import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import Components from 'unplugin-vue-components/vite';
 import { PrimeVueResolver } from '@primevue/auto-import-resolver';
+
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    Components({
-      resolvers: [
-        PrimeVueResolver()
-      ]
-    }),
-    vueJsx(),
-    vueDevTools(),
-  ],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, '/api'), // Garde /api dans l'URL
-      }
+export default defineConfig(({ mode }) => {
+  // Charger les variables d'environnement
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [
+      vue(),
+      Components({
+        resolvers: [
+          PrimeVueResolver()
+        ]
+      }),
+      vueJsx(),
+      vueDevTools(),
+    ],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_BACKEND_URL,
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/api/, '/api'),
+        }
+      },
+      hmr: {
+        protocol: 'ws',
+        host: 'localhost'
+      },
     },
-    hmr: {
-      protocol: 'ws',
-      host: 'localhost'
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
     },
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+  }
 })
